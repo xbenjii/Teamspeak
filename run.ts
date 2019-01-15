c6ddb7942cbfcda0b4df1641f034c51e595c09a1
Ben Fortune <benjamin.fortune1@gmail.com>
2019-01-15 11:20:14 +0000

    normalize the connection, add connect method

--- run.ts
+++ run.ts
@@ -1,7 +1,7 @@
 import { Teamspeak, TeamspeakOptions, TeamspeakConnectionType } from './src';
 
 const ts3 = new Teamspeak(<TeamspeakOptions>{
-    connectionType: TeamspeakConnectionType.TelnetConnection,
+    connectionType: TeamspeakConnectionType.Telnet,
     host: 'localhost',
     port: 10011
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { TelnetConnection, SSHConnection } from './Connections';
 
 export enum TeamspeakConnectionType {
-    TelnetConnection,
-    SSHConnection
+    Telnet,
+    SSH
+};
+
+export const TeamspeakConnection = {
+    [TeamspeakConnectionType.Telnet]: TelnetConnection,
+    [TeamspeakConnectionType.SSH]: SSHConnection
 };
 
 export type TeamspeakOptions = {
@@ -18,13 +23,11 @@ export class Teamspeak {
 
     constructor(options: TeamspeakOptions) {
         this.options = options;
-        if(options.connectionType === TeamspeakConnectionType.TelnetConnection) {
-            this.connection = new TelnetConnection({ host: options.host, port: options.port});
-        } else if(options.connectionType === TeamspeakConnectionType.SSHConnection) {
-            this.connection = new SSHConnection({ host: options.host, port: options.port});
-        }
-        this.connection.connect();
+        this.connection = new TeamspeakConnection[options.connectionType]({ host: options.host, port: options.port });
     }
 
+    connect(): Promise<any> {
+        return this.connection.connect();
+    }
 
 }
